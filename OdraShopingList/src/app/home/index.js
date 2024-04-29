b55e6acdfd95bb8e557ea1a9dc38362f2b14a893
eduardo.viegas <eduardo.viegas@etec.sp.gov.br>
2024-04-29 12:25:41 -0300

    Criando a página de listagem de produtos

--- OdraShopingList/src/app/home/index.js
+++ OdraShopingList/src/app/home/index.js
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+  TextImput,
+  touchableOpacity
+} from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Ionicons } from '@expo/vector-Icons'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Página Inicial</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        source={require('../../assets/background.jpg')}
+        resizeMode='repeat'
+        style={{ flex: 1, justfyContend: 'flex-start' }}
+      >
+        <View styles={styles.header}>
+          <Text style={styles.title}> Lista de produtos </Text>
+          <Ionicons name="trash" size={32} color='#fff' />
+        </View>
+
+      <View style={styles.footer}>
+
+      </View>
+      </ImageBackground>
+      <Statusbar style="light" BackgroundColor='#000' />
+    </SafeAreaView>
   )
 }
 
